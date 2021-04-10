cc268eb47b0d3e0b329e1ec1624526e9cd122a96
tiger <qwerttiger123456789@gmail.com>
2021-04-10 13:09:31 -0400

    add gold

--- prestige clicker.py
+++ prestige clicker.py
@@ -2,3 +2,16 @@ import pygame
 pygame.init()
 screen=pygame.display.set_mode([700,700])
 pygame.display.set_caption("Prestige Clicker")
+screen.fill((255,255,255))
+pygame.draw.rect(screen,(0,0,0),pygame.Rect(200,50,300,75),2)
+gold=0
+while True:
+  for event in pygame.event.get():
+    if event.type==pygame.MOUSEBUTTONDOWN:
+      pos=pygame.mouse.get_pos()
+      if 200<=pos[0]<=500 and 50<=pos[1]<=125:
+        gold+=1
+    if event.type==pygame.QUIT:
+      pygame.quit()
+  pygame.display.flip()
+  print(gold)
