08648e5c375c2746b5f73407476ab20044094a0f
tiger <qwerttiger123456789@gmail.com>
2021-04-16 13:18:15 -0400

    make it better

--- prestige clicker.py
+++ prestige clicker.py
@@ -5,7 +5,9 @@ pygame.init()
 screen=pygame.display.set_mode([700,700])
 pygame.display.set_caption("Prestige Clicker")
 points=dec(0)
+totalpoints=dec(0)
 metapoints=0
+totalmetapoints=0
 unlockedmeta=False
 wscreen=0
 def drawtext(text,pos,size=30,colour=(0,0,0)):
@@ -19,8 +21,10 @@ while True:
       pos=pygame.mouse.get_pos()
       if 200<=pos[0]<=500 and 150<=pos[1]<=225 and wscreen==0:
         points+=1+dec(metapoints)/10
+        totalpoints+=1+dec(metapoints)/10
       if 200<=pos[0]<=500 and 475<=pos[1]<=550 and unlockedmeta and points>=25 and wscreen==0:
-        metapoints+=math.floor(math.sqrt(dec(points))/5)
+        metapoints+=math.floor(math.sqrt(totalpoints)/5)-totalmetapoints
+        totalmetapoints+=math.floor(math.sqrt(totalpoints)/5)-totalmetapoints
         points=0
       if pos[1]<50:
         if pos[0]<=233:
@@ -45,8 +49,9 @@ while True:
     if unlockedmeta:
       pygame.draw.rect(screen,(0,0,0),pygame.Rect(200,475,300,75),2)
       if points>=25:
-        drawtext("+"+str(math.floor(math.sqrt(points)/5))+" Meta-Points",(350,513))
+        drawtext("+"+str(math.floor(math.sqrt(totalpoints)/5)-totalmetapoints)+" Meta-Points",(350,513))
       else:
         drawtext("+0 Meta-Points",(350,513))
       drawtext(str(metapoints)+" Meta-Points",(350,610),80)
+  print(totalpoints)
   pygame.display.flip()
